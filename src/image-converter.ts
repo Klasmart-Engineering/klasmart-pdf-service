e57fdeb46a77cd1581bd7f432eafe67b7e8ede37
Mitch Goshorn <mitch.goshorn@calmid.com>
2021-06-11 11:13:46 +0900

    Added logging integration. Added environment variable to define default logging level. Replaces all console.log references with winston logger usage

--- src/image-converter.ts
+++ src/image-converter.ts
@@ -1,30 +1,32 @@
 import { Canvas, JPEGStream } from 'canvas';
 import * as pdf from 'pdfjs-dist/es5/build/pdf';
 import { PDFDocumentProxy } from 'pdfjs-dist/types/display/api';
+import { withLogger } from './logger';
+
+const log = withLogger('image-converter');
 
 export const createDocumentFromStream = async (pdfUrl: string): Promise<PDFDocumentProxy> => {
-    console.log('creating document');
+    log.debug('creating document');
     return pdf.getDocument(pdfUrl).promise;
 }
 
 export const generatePageImage = async (document: PDFDocumentProxy, pageNumber: number): Promise<JPEGStream> => {
-    console.log(`creating page: ${pageNumber}/${document.numPages}`)
+    log.debug(`creating page: ${pageNumber}/${document.numPages}`)
     const pageProxy = await document.getPage(pageNumber);
 
-    console.log('creating viewport/canvas')
+    log.debug('creating viewport/canvas')
     const viewport = pageProxy.getViewport({scale: 3});
     const canvas = new Canvas(viewport.width, viewport.height, "image");
 
-    console.log('rendering page to canvas');
+    log.debug('rendering page to canvas');
     await pageProxy.render({
         canvasContext: canvas.getContext('2d'),
         viewport: viewport
     }).promise;
 
-    console.log('creating jpeg output stream');
+    log.debug('creating jpeg output stream');
     const imageOutputStream = canvas.createJPEGStream({
         quality: .99
     });
-    console.log('Resolving')
     return imageOutputStream;
 }
