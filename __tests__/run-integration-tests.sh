a6830d49a67252c69317bd8ec873c772ba328848
Mitch Goshorn <mitch.goshorn@calmid.com>
2021-07-09 13:50:52 +0900

    Added integration test shell script to start S3 ninja prior to integration test run

---
+++ __tests__/run-integration-tests.sh
@@ -0,0 +1,21 @@
+# Startup S3 ninja
+echo "Starting s3-ninja Container"
+docker pull scireum/s3-ninja:7.0
+S3_NINJA_HASH=$(docker run -d -p 9872:9000 scireum/s3-ninja:7.1.1) 
+
+echo "Waiting for S3-Ninja startup"
+sleep 10
+
+# Configure ENV variables for S3 Ninja usage
+
+export AWS_SECRET_KEY=$(docker exec $S3_NINJA_HASH cat /home/sirius/app/application.conf | grep "awsSecretKey" | grep -o '".*"' | sed 's/"//g')
+export AWS_SECRET_KEY_NAME=$(docker exec $S3_NINJA_HASH cat /home/sirius/app/application.conf | grep "awsAccessKey" | grep -o '".*"' | sed 's/"//g')
+export AWS_REGION=ap-northeast-2
+export AWS_BUCKET=kidsloop-pdf-pages-integration-tests
+export AWS_S3_HOST=http://localhost:9872
+
+# Run tests
+npm run test:integration
+
+# Clean up
+docker stop $S3_NINJA_HASH
