235c9721670e376d3472fdd8a73648afc2a0137f
Mitch Goshorn <mitch.goshorn@calmid.com>
2021-07-20 16:50:14 +0900

    Updated post-install script to not manage gulp installation, added local gulp install to package.json - hopefully should solve issues with post-install scrip tnot running properlly during test phase of pipeline

--- post-install.sh
+++ post-install.sh
@@ -1,10 +1,3 @@
-npm install -g gulp
-npm link gulp
-npm link gulp-less
-npm link gulp-watch
-npm link gulp-autoprefixer
-npm link gulp-plumber
-npm link path
 git clone https://github.com/mozilla/pdf.js.git temp-pdf.js
 git --git-dir=./temp-pdf.js/.git checkout 07955fa1d31df62fd668a15f58cd40b29a82bd63
 npm --prefix ./temp-pdf.js/ install ./temp-pdf.js/
