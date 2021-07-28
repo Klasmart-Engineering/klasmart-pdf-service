820ffd8778aa30b9aee46c2b98e6844bd7456320
Mitch Goshorn <mitch.goshorn@calmid.com>
2021-07-28 11:48:25 +0900

    Adds manual canvas build to post-install script

--- post-install.sh
+++ post-install.sh
@@ -3,4 +3,9 @@ git --git-dir=./temp-pdf.js/.git checkout 07955fa1d31df62fd668a15f58cd40b29a82bd
 npm --prefix ./temp-pdf.js/ install ./temp-pdf.js/
 gulp --cwd=./temp-pdf.js/ dist-install
 cp -r ./temp-pdf.js/build/dist ./node_modules/pdfjs-dist
+git clone https://github.com/Automattic/node-canvas canvas
+npm --prefix ./canvas// install ./canvas/
+rm -rf ./node_modules/canvas
+mv ./canvas ./node_modules/
+
 rm -rf ./temp-pdf.js
