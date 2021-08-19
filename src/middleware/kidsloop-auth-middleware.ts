b2cc86eeb6d2c4f36a7ad1e90d236a52c3b1afcb
Mitch Goshorn <mitch.goshorn@calmid.com>
2021-08-19 11:33:29 +0900

    Added doc comment for authentication middleware

--- src/middleware/kidsloop-auth-middleware.ts
+++ src/middleware/kidsloop-auth-middleware.ts
@@ -5,6 +5,18 @@ import { AuthType } from './Access';
 
 const log = withLogger('kidsloop-auth-middleware');
 
+/**
+ * Creates middleware function that authenticates users based on Kidsloop access token
+ * Token processing is handled by the kidsloop-token-validation module. Based on the token
+ * validation an AuthType will be registered on response.locals for use in later processing.
+ * 
+ * At the current time the access token does not seem to include any privilege or access level
+ * information so it only assigns AuthType.Authenticated and AuthType.Anonymous.
+ * 
+ * In development environments, the environment variable DEV_JWT_SECRET can be registered for 
+ * testing JWT integration, but in the production environment the JWT secret is provided by the
+ * kidsloop-token-validation module.
+ */
 export function kidsloopAuthMiddleware(): (request: Request, response: Response, next: NextFunction) => void {
 
     if (process.env.DEV_JWT_SECRET) {
