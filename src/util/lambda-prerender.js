697a6b7e78380578df072b711936b8a2974efcf9
Mitch Goshorn <mitch.goshorn@calmid.com>
2021-11-25 10:00:40 +0900

    fix(lambda): fix lambda logic to prevent premature termination of lambda session

--- src/util/lambda-prerender.js
+++ src/util/lambda-prerender.js
@@ -1,21 +1,30 @@
-const http = require('http');
+const https = require('https');
 
-exports.handler = async (event, context, callback) => {
-    const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
-    if (key.endsWith('.pdf')) {
-        http.get(`${process.env.PDF_SERVICE}/pdf/${key}/prerender`, (res) => {
+exports.handler = (event, context, callback) => {
+    const fullKey = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
+    console.log(`Full Key: ${fullKey}`);
+    const key = fullKey.split('/').pop();
+    console.log(`Operational part of key: ${key}`)
+    if (fullKey.startsWith('assets') && key.endsWith('.pdf')) {
+        const address = `${process.env.PDF_SERVICE}/pdf/${key}/prerender`;
+        console.log(`Sending webhook to: ${address}`)
+        https.get(address, (res) => {
             callback(null, res.statusCode)
         })
         .on('error', (err) => {
+            console.log('error', err)
             callback(err)
         })
         .on('abort', (err) => {
+            console.log('abort', err)
             callback(err);
         })
         .on('timeout', () => {
+            console.log('timeout');
             callback(new Error('timeout'));
         });
     } else {
+        console.log('Non operational key');
         callback(null, 204);
     }
 }
