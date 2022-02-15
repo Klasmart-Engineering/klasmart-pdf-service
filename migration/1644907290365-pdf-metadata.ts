b8de87d1c666e03937825cfe98166abafa6d791c
Mitch Goshorn <mitch.goshorn@kidsloop.live>
2022-02-15 15:42:31 +0900

    chore(KLL-2903): add orm config, add migration script for current database changes

---
+++ migration/1644907290365-pdf-metadata.ts
@@ -0,0 +1,16 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class pdfMetadata1644907290365 implements MigrationInterface {
+    name = 'pdfMetadata1644907290365'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "pdf_metadata" ADD "outline" jsonb`);
+        await queryRunner.query(`ALTER TABLE "pdf_metadata" ADD "pageLabels" jsonb`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "pdf_metadata" DROP COLUMN "pageLabels"`);
+        await queryRunner.query(`ALTER TABLE "pdf_metadata" DROP COLUMN "outline"`);
+    }
+
+}
---
+++ ormconfig.ts
@@ -0,0 +1,22 @@
+import { PDFMetadata } from './src/models/PDFMetadata';
+
+export default {
+    name: 'default',
+    type: 'postgres',
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER,
+    port: +(process.env.DB_PORT || 5432),
+    database: process.env.DB_DATABASE || 'postgres',
+    password: process.env.DB_PASSWORD,
+    entities: [ PDFMetadata ],
+
+    migrations: [ 'migration/*.{js,ts}' ],
+    cli: {
+        migrationsDir: 'migration'
+    },
+
+    logging: 'all',
+    extra: {
+      connectionLimit: 5
+    }
+}
