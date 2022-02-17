9a53c0bfec2a7bf0bdd0ad06d36beb56f1987d5c
Mitch Goshorn <mitch.goshorn@kidsloop.live>
2022-02-17 10:23:56 +0900

    chore(KLL-2903): update migration to only add columns if they do not already exist

--- migration/1644907290365-pdf-metadata.ts
+++ migration/1644907290365-pdf-metadata.ts
@@ -4,8 +4,8 @@ export class pdfMetadata1644907290365 implements MigrationInterface {
     name = 'pdfMetadata1644907290365'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "pdf_metadata" ADD "outline" jsonb`);
-        await queryRunner.query(`ALTER TABLE "pdf_metadata" ADD "pageLabels" jsonb`);
+        await queryRunner.query(`ALTER TABLE "pdf_metadata" ADD COLUMN IF NOT EXISTS "outline" jsonb`);
+        await queryRunner.query(`ALTER TABLE "pdf_metadata" ADD COLUMN IF NOT EXISTS "pageLabels" jsonb`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
